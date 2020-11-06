6f54c341409c0cf079295118996a2a8a8248f617
Ladivcr <vrosas832@gmail.com>
2020-11-06 12:27:43 -0600

    Lo visto durante la segunda parte de la clase de modelado

---
+++ modeling.py
@@ -0,0 +1,43 @@
+#!#/usr/bin/env python3
+
+from sympy import *
+
+x = Symbol("x") # Definimos x como simbolo
+
+formulae = 'x^3+1.0+sin(x)' # Definimos la función 
+
+# Esto ya no es str sino es una función simbolica en la
+# que x es un simbolo 
+y = sympify(formulae) # Hacemos la función simbolica
+
+# Tomamos la función y 
+"""
+.diff es el operador derivada y lo que le pasamos como 
+argumento es respecto a lo que vamos a derivar
+en este caso es x, derivamos respecto a x
+"""
+yprime = y.diff(x)
+
+print (y)
+print (yprime)
+
+# lambdify lo que hace es tomar 
+# y como función y su evaluación respecto a x
+# evaluar con numpy 
+"""
+lambdifit toma una expresión simbolica y la vuelve una función 
+cuyo equivalente es algo como: 
+    def f(x):
+        y = 2*x**2
+        return(y)
+"""
+f = lambdify(x,y,"numpy")
+# Evaluamos en determinado valor
+x0 = 1.5
+y0 = f(x0)
+
+print(y0)
+
+f1 = lambdify(x,yprime,"numpy")
+y1 = f1(x0)
+print(y1)
