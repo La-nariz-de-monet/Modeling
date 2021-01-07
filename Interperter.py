b9ceeea20ca54e214842b20b206321783ad49dd9
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2021-01-07 14:35:19 -0600

    Interpreter Turing Machine

--- Interperter.py
+++ Interperter.py
@@ -6,16 +6,37 @@ for line in rfile:
     states = line.split(' ')
     if(states != ['\n']):
         read, write, move = states[1:4]
+        if(write == '_'):
+            write = ' '
+        if(read == '_'):
+            read = ' '
         next = states[4][:-1]
-        print(states,read,write,move,next)
         if( states[0] in rules ):
             rules[states[0]][read] = { 'write' : write, 'move' : move, 'next': next }
         else:
             rules[states[0]] = { read : {} }
         rules[states[0]][read] = { 'write' : write, 'move' : move, 'next': next }
 
-finish = false
-tape = list('  1 1   ')
-head = 3
+finish = False
+head = 2
+state = '0'
+print("Write two binary numbers separated by a space")
+line = input()
+tape = list('  '+str(line)+'  ')
 
-while(!finish):
+while(not(finish)):
+    print('State:',state,', head:', head-2, ', tape:', ''.join(tape))
+    read = tape[head]
+    write = rules[state][read]['write']
+    move = rules[state][read]['move']
+    next = rules[state][read]['next']
+    tape[head] = write
+    if(move == 'r'):
+        head += 1
+    elif(move == 'l'):
+        head -=1
+    state = next
+    if state == 'halt-accept':
+        finish = True
+
+print('\nFinish head:', head, ', tape:', ''.join(tape))
